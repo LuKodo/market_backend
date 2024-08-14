eb591edfeb0a95313627f2e9625f3c64496e1f24
Luis Caraballo <lcm7039@gmail.com>
2024-08-14 15:38:44 -0500

    last

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import serverless from 'serverless-http';
+import express from 'express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+const server = express();
+export const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Market Store API')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
-  await app.listen(8080);
-}
-bootstrap();
+  return app.init();
+};
+
+createNestServer(server).then(() => {
+  console.log('Nest Ready');
+}).catch((err) => {
+  console.error('Nest broken', err);
+})
+
+export const handler = serverless(server)
