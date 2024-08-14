eac1486d38b93dc1c40354cb8b41750133e065de
Luis Caraballo <lcm7039@gmail.com>
2024-08-14 16:30:32 -0500

    last

--- Discovery.md
+++
@@ -1,9 +0,0 @@
----
-app_name: MkStoreBack
-tagline: MkStore
-media: []
-ported_from: ""
-works_with: []
----
-
-
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import serverless from 'serverless-http';
-import express from 'express';
 
-const server = express();
-export const createNestServer = async (expressInstance) => {
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressInstance),
-  );
-  app.enableCors();
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
     .setTitle('Market Store API')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
-  return app.init();
-};
-
-createNestServer(server).then(() => {
-  server.listen(80);
-  console.log('Nest Ready');
-}).catch((err) => {
-  console.error('Nest broken', err);
-})
-
-export const handler = serverless(server)
+  SwaggerModule.setup('api', app, document);
+  app.enableCors();
+  await app.listen(80);
+}
+bootstrap();
