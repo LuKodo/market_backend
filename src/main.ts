767f75b91b21998cbcf13af36a715f160d521a1a
Luis Caraballo <lcm7039@gmail.com>
2024-08-14 15:42:40 -0500

    ok

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,7 @@ export const createNestServer = async (expressInstance) => {
 };
 
 createNestServer(server).then(() => {
+  server.listen(3000);
   console.log('Nest Ready');
 }).catch((err) => {
   console.error('Nest broken', err);
